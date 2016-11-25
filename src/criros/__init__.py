3a4ab1f03e5fd08702093e92f9f3ec239bbd7d9c
fsuarez6 <fsuarez6@gmail.com>
2016-11-25 17:53:05 +0800

    Add filters module

--- src/criros/__init__.py
+++ src/criros/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import conversions
 import exploration
+import filters
 import raveutils
 import spalg
 from utils import read_parameter, read_parameter_err, read_parameter_fatal
